70c0f2e21883dcbdaa6c3d1ac43a933504183a7f
Sobir <sobirbakhtiyor@gmail.com>
2017-02-20 23:45:20 +0400

    Add grid system & style example

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to Github</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Tashkent'>Tashkent</Link>
+        </li>
+        <li>
+          <Link to='/?location=Zaamin'>Zaamin</Link>
+        </li>
+      </ol>
     </div>
   )
 };
--- public/bundle.js
+++ public/bundle.js
@@ -25201,11 +25201,14 @@
 	    null,
 	    React.createElement(Nav, null),
 	    React.createElement(
-	      'h2',
-	      null,
-	      'Main Component'
-	    ),
-	    props.children
+	      'div',
+	      { className: 'row' },
+	      React.createElement(
+	        'div',
+	        { className: 'columns medium-6 large-4 small-centered' },
+	        props.children
+	      )
+	    )
 	  );
 	};
 
@@ -26925,19 +26928,44 @@
 
 	var React = __webpack_require__(8);
 
+	var _require = __webpack_require__(166),
+	    Link = _require.Link;
+
 	var Examples = function Examples(props) {
 	  return React.createElement(
 	    'div',
 	    null,
 	    React.createElement(
-	      'h3',
-	      null,
+	      'h1',
+	      { className: 'text-center' },
 	      'Examples'
 	    ),
 	    React.createElement(
 	      'p',
 	      null,
-	      'Welcome to Github'
+	      'Here are a few example to try out:'
+	    ),
+	    React.createElement(
+	      'ol',
+	      null,
+	      React.createElement(
+	        'li',
+	        null,
+	        React.createElement(
+	          Link,
+	          { to: '/?location=Tashkent' },
+	          'Tashkent'
+	        )
+	      ),
+	      React.createElement(
+	        'li',
+	        null,
+	        React.createElement(
+	          Link,
+	          { to: '/?location=Zaamin' },
+	          'Zaamin'
+	        )
+	      )
 	    )
 	  );
 	};
