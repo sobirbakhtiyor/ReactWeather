b5130db5dfcd91425eb214ab5a40c8aac1e4e54e
one <one>
2017-02-20 20:29:36 +0400

    Initial commit

---
+++ app/components/Examples.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Examples = (props) => {
+  return (
+    <div>
+      <h3>Examples Component!</h3>
+      <p>Welcome to Github!</p>
+    </div>
+  )
+};
+
+module.exports = Examples;
