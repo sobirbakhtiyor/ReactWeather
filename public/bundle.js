6d4f51bf5ec55fd5b6adb0e1c698cb47714580f0
Sobir <sobirbakhtiyor@gmail.com>
2017-02-20 21:44:29 +0400

    Rename about page

--- public/bundle.js
+++ public/bundle.js
@@ -26791,9 +26791,18 @@
 
 	var About = function About(props) {
 	  return React.createElement(
-	    'h3',
+	    'div',
 	    null,
-	    'About Component'
+	    React.createElement(
+	      'h3',
+	      null,
+	      'About Component'
+	    ),
+	    React.createElement(
+	      'p',
+	      null,
+	      'Welcome to about page!'
+	    )
 	  );
 	};
 
